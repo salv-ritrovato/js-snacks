b272721c8a636b90d93a87710c1f1521e9754cc8
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-05 10:50:43 +0100

    added new folders

---
+++ snack-4/script.js
@@ -0,0 +1,13 @@
+console.log('Io odio JS')
+// FUNZIONA!!
+
+// L'UTENTE INSERISCE DUE NUMERI IN SUCCESSIONE CON DUE PROMPT. IL SOFTWARE STAMPA IL MAGGIORE.
+let number1 = prompt('Enter first number');
+let number2 = prompt('Enter second number');
+console.log(number1, number2)
+
+if (number1 > number2) {
+    console.log(number1)
+} else {
+    console.log(number2)
+}
