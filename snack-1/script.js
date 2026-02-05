e043a3f46df8536ee9f02ac15efe44dd1c56829a
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-05 10:31:22 +0100

    first commit

---
+++ snack-1/script.js
@@ -0,0 +1,7 @@
+console.log('Io odio JS')
+// FUNZIONA!!
+
+// L'UTENTE INSERISCE DUE NUMERI IN SUCCESSIONE CON DUE PROMPT. IL SOFTWARE STAMPA IL MAGGIORE.
+let number1 = prompt('Enter first number');
+let number2 = prompt('Enter second number');
+console.log(number1, number2)
---
+++ snack-3/script.js
@@ -0,0 +1,2 @@
+console.log('Io odio JS')
+// FUNZIONA!!
