fa313f002bf25a896137e4e855fc02ce7d583ce4
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-05 12:52:15 +0100

    fixed snack3

--- snack-3/script.js
+++ snack-3/script.js
@@ -4,7 +4,7 @@ console.log('Io odio JS')
 // IL SOFTWARE DEVE CHIEDERE PER 10 VOLTE ALL'UTENTE DI INSERIRE UN NUMERO. IL PROGRAMMA STAMPA LA SOMMA DI TUTTI I NUMERI INSERITI.
 let sum = 0;
 for (let i = 1; i <= 10; i++) {
-    let numbers = Number(prompt('Insert 10 numbers')); // utilizzo Number per convertire stringa in numeri
+    let numbers = Number(prompt('Insert a number')); // utilizzo Number per convertire stringa in numeri
     console.log(i);
     sum = sum + numbers;
 }
