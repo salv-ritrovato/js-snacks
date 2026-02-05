52e82f10d9040b66bc4eada51787f25e6c27a06d
salv-ritrovato <salv.ritrovato@gmail.com>
2026-02-05 11:34:02 +0100

    snack2

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,12 +2,14 @@ console.log('Io odio JS')
 // FUNZIONA!!
 
 // L'UTENTE INSERISCE DUE NUMERI IN SUCCESSIONE CON DUE PROMPT. IL SOFTWARE STAMPA IL MAGGIORE.
-let number1 = prompt('Enter first number');
+let number1 = prompt('Enter first number'); // potevo usare Number(prompt(Enter first number)) per convertire subito la stringa in numeri
 let number2 = prompt('Enter second number');
 console.log(number1, number2)
 
 if (number1 > number2) {
     console.log(number1)
-} else {
+} else if (number2 > number1) {
     console.log(number2)
-}
+} else {
+    console.log('Numbers are =')
+    }
--- snack-2/script.js
+++ snack-2/script.js
@@ -1,2 +1,16 @@
 console.log('Io odio JS')
 // FUNZIONA!!
+
+// L'UTENTE INSERISCE DUE PAROLE IN SUCCESSIONE CON DUE PROMPT. IL SOFTWARE STAMPA PRIMA LA PIU' CORTA E POI LA PIU' LUNGA.
+let word1 = prompt('Enter first word');
+let word2 = prompt('Enter second word');
+console.log(word1, word2)
+
+if (word2.length > word1.length) {
+    console.log(word1 + " " + word2
+    )
+} else if (word1.length < word2.length) {
+    console.log(word1 + " " + word2)
+} else {
+    ( console.log('The words are equal');)
+}
